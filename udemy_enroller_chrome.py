3eab4ad739d90c2102ed168ab6463a044fc6ed08
Aman Verasia <verasiaaman3@gmail.com>
2020-11-07 15:01:50 +0530

    Update udemy_enroller_chrome.py

--- udemy_enroller_chrome.py
+++ udemy_enroller_chrome.py
@@ -1,6 +1,5 @@
 # Install all the requirements by running requirements.py in IDLE or follow the alternate instructions at
-# https://github.com/aapatre/Automatic-Udemy-Course-Enroller-GET-PAID-UDEMY-COURSES-for-FREE/ Make sure you have
-# cleared all saved payment details on your Udemy account & the browser!
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
