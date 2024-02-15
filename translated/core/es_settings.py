5c3759d74cadee1c4a54257e80190dd488491aeb
Aman Verasia <verasiaaman3@gmail.com>
2024-02-15 14:41:24 +0530

    New translations settings.py (Spanish)

--- translated/core/es_settings.py
+++ translated/core/es_settings.py
@@ -1,150 +1,150 @@
-import getpass
-import os.path
-from distutils.util import strtobool
-from typing import Dict
-from typing import List
+importar getpass
+importar os.path
+desde distutils.util importar strtobool
+desde el escribir importar Dict
+desde la lista de importación
 
-from ruamel.yaml import dump
-from ruamel.yaml import YAML
+desde el volcado de importación de ruamel.yaml
+desde ruamel.yaml importar YAML
 
 
-class Settings:
+configuración de clase:
     """
-    Contains all logic related to the scripts settings
+    Contiene toda la lógica relacionada con la configuración de scripts
     """
 
     def __init__(self):
-        self.email = None
-        self.password = None
-        self.zip_code = None
+        self.email = Ninguno
+        self.password = Ninguno
+        self.zip_code = Ninguno
         self.languages = []
 
         self._settings_path = "settings.yaml"
         self.is_ci_build = strtobool(os.environ.get("CI", "False"))
         self._init_settings()
 
-    def _init_settings(self) -> None:
+    def _init_settings(self) -> Ninguno:
         """
-        Initialize the settings to be used in the script
+        Inicializar la configuración a usar en el script
 
         :return:
         """
         if self.is_ci_build:
             self._load_ci_settings()
-        else:
+        no:
             settings = self._load_user_settings()
-            if settings is None:
-                self._generate_settings()
+            si la configuración no es ninguna:
+                auto._generar_configuración()
                 self._save_settings()
 
     def _load_ci_settings(self):
         """
-        Load environment variables for CI run
+        Cargar variables de entorno para ejecutar CI
 
         :return:
         """
-        print("Loading CI settings")
+        print("Cargando ajustes de IC")
         self.email = os.environ["UDEMY_EMAIL"]
         self.password = os.environ["UDEMY_PASSWORD"]
 
-    def _load_user_settings(self) -> Dict:
+    def _load_user_settings(self) -> Dictar:
         """
-        Loads the settings from the yaml file if it exists
+        Carga la configuración desde el archivo yaml si existe
 
-        :return: dictionary containing the script settings
+        Diccionario :return: que contiene la configuración del script
         """
         yaml = YAML()
 
-        settings = None
-        if os.path.isfile(self._settings_path):
-            print("Loading existing settings")
-            with open(self._settings_path) as f:
-                settings = yaml.load(f)
-            udemy_settings = settings["udemy"]
-            self.email = udemy_settings["email"]
-            self.password = udemy_settings["password"]
+        ajustes = Ninguno
+        si os.path.isfile(self._settings_path):
+            print("Cargando ajustes existentes")
+            con open(self._settings_path) como f:
+                ajustes = yaml.load(f)
+            udemy_settings = ajustes["udemy"]
+            self.email = configuración de udemy["email"]
+            self.password = configuración de udemy["contraseña"]
             self.zip_code = udemy_settings.get("zipcode")
-            self.languages = udemy_settings.get("languages")
-        return settings
+            self.languages = udemy_settings.get("idiomas")
+        regresar ajustes
 
-    def _generate_settings(self) -> None:
+    def _generate_settings(self) -> Ninguno:
         """
-        Generate the settings for the script
+        Generar la configuración del script
 
         :return:
         """
-        self.email = self._get_email()
+        self.email = self._obtener_email()
         self.password = self._get_password()
         self.zip_code = self._get_zip_code()
         self.languages = self._get_languages()
 
     def _get_email(self) -> str:
         """
-        Get input from user on the email to use for udemy
+        Obtener entrada del usuario en el correo electrónico para usar para udemy
 
-        :return: The users udemy email
+        :return: El correo udemy de los usuarios
         """
-        email = input("Please enter your udemy email address: ")
+        email = input("Por favor, introduce tu dirección de correo electrónico de udemy: ")
         if len(email) == 0:
-            print("You must provide your email")
-            return self._get_email()
-        return email
+            print("Debes proporcionar tu correo electrónico")
+            devolver self._get_email()
+        correo de devolución
 
     def _get_password(self) -> str:
         """
-        Get input from user on the password to use for udemy
+        Obtener entrada del usuario en la contraseña para usar para udemy
 
-        :return: The users udemy password
+        :return: La contraseña udemy de los usuarios
         """
-        password = getpass.getpass(prompt="Please enter your udemy password: ")
-        if len(password) == 0:
-            print("You must provide your password")
-            return self._get_password()
-        return password
+        password = getpass.getpass(prompt="Por favor, introduzca su contraseña de udemy: ")
+        if len(contraseña) == 0:
+            print("Debe proporcionar su contraseña")
+            devolver self._get_password()
+        devolver contraseña
 
     @staticmethod
     def _get_zip_code() -> str:
         """
-        Get input from user on the zip code to use for udemy
+        Obtener entrada del usuario en el código postal para usar para udemy
 
-        :return: The users udemy zip code
+        :return: El código postal de los usuarios
         """
         zip_code = input(
-            "Please enter your zipcode (Not necessary in some regions): ")
-        return zip_code
+            "Por favor, introduzca su código postal (no necesario en algunas regiones): ")
+        devolver código postal
 
     @staticmethod
-    def _get_languages() -> List:
+    def _get_languages() -> Lista:
         """
-        Get input from user on the languages they want to get courses in
+        Obtén información del usuario sobre los idiomas en los que quiere obtener los cursos
 
-        :return: list of languages the user wants to redeem udemy courses in
+        Lista :return: de idiomas en los que el usuario quiere canjear cursos de udemy
         """
-        languages = input(
-            "Please enter your language preferences (comma separated list e.g. English,German): "
+        idiomas = entrada(
+            "Por favor, introduzca sus preferencias de idioma (lista separada por comas por ejemplo, Inglés, Alemán): "
         )
         return [lang.strip()
                 for lang in languages.split(",")] if languages else []
 
-    def _save_settings(self) -> None:
+    def _save_settings(self) -> Ninguno:
         """
-        Confirm if the user wants to save settings to file
+        Confirmar si el usuario quiere guardar la configuración en el archivo
 
         :return:
         """
         yaml_structure = dict()
-        save_settings = input(
-            "Do you want to save settings for future use (Y/N): ")
+        save_settings = entrada(
+            "Desea guardar la configuración para uso futuro (S/N): ")
         if save_settings.lower() == "y":
             yaml_structure["udemy"] = {
                 "email": str(self.email),
-                "password": str(self.password),
+                "contraseña": str(self.password),
                 "zipcode": str(self.zip_code),
                 "languages": self.languages,
             }
 
-            with open(self._settings_path, "w+") as f:
+            con open(self._settings_path, "w+") como f:
                 dump(yaml_structure, stream=f)
-            print(f"Saved your settings in {self._settings_path}")
-        else:
-            print("Not saving your settings as requested")
+            imprime(f"Guardado tus ajustes en {self._settings_path}")
+        no:
+            print("No se guardan los ajustes como se solicita")
