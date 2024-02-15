bc0a3167a5f8f96e2e750fa5e2a5b502a14dc9bb
Aman Verasia <verasiaaman3@gmail.com>
2024-02-15 14:41:34 +0530

    New translations get_links.py (Spanish)

--- translated/es_get_links.py
+++ translated/es_get_links.py
@@ -1,24 +1,24 @@
-from selenium.webdriver.remote.webdriver import WebDriver
+desde selenium.webdriver.remote.webdriver importar WebDriver
 
-from core import TutorialBarScraper
-links = []
+desde el núcleo importar TutorialBarScraper
+enlaces = []
 tb_scraper = TutorialBarScraper()
 
-print('1. IT software')
-print('2. Development')
-print('3. Finance Accounting')
-print('4. Design')
-print('5. Business')
+print('1. Software IT')
+print('2. Desarrollo')
+print('3. Contabilidad financiera')
+print('4. Diseño)
+print('5. Negocios)
 print('6. Marketing')
-print('7. Health and Fitness')
-print('8. Office Productivity')
-print('9. Photography')
-print('10. Personal Development')
-print('11. Teaching and Academics')
-print('99. All Courses')
-choice = input('Please select what category you want? :')
-while True:
-    udemy_course_links = tb_scraper.run(choice)
-    links = links + udemy_course_links
-print(links)
+print('7. Salud y Fitness ')
+print('8. Productividad de oficina')
+print('9. Fotografía')
+print('10. Desarrollo personal')
+print('11. Enseñanza y académicos')
+print('99. Todos los cursos')
+choice = input('Por favor seleccione qué categoría quiere? :')
+mientras verdadera:
+    udemy_course_links = tb_scraper.run(elección)
+    enlaces = enlaces + enlaces udemy_course_links
+impresión(enlaces)
 print(len(links))
